36579e5de2a082d849aaeb89ed3d7e6b90ac176f
mihail.gaberov <mihail.gaberov@ysura.com>
2021-12-30 13:32:04 +0100

    :sparkles: Add Feeling Lucky functionality

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,30 @@ function App() {
 
   const fetchPage = (pageNum: number) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=${pageNum}&with_watch_monetization_types=flatrate`)
-    .then(response => response.json())
-    .then(data => setMovies(data.results))
+      .then(response => response.json())
+      .then(data => setMovies(data.results))
   }
 
   const showMostPopular = () => {
     fetchPage(1)
   }
-  
+
   const showMostLuckyMatch = () => {
-    fetch(`https://api.themoviedb.org/3/movie/122?api_key=${API_KEY}&language=en-US`)
-    .then(response => response.json())
-    .then(data => setMovies([data])
-    )
+    // Generate a random ID betwee min and max values
+    const min: number = 1
+    const max: number = 1000
+    const randomId: number = Math.floor(Math.random() * (max - min + 1) + min)
+
+    fetch(`https://api.themoviedb.org/3/movie/${randomId}?api_key=${API_KEY}&language=en-US`)
+      .then(response => response.json())
+      .then(data => setMovies([data])
+      )
   }
 
   const clearSelection = () => setMovies([])
 
   const showPrevChunk = () => {
-    if(page > 1) {
+    if (page > 1) {
       setPage(prevPageNum => {
         const newPageNum = prevPageNum - 1
         fetchPage(newPageNum)
@@ -51,8 +56,8 @@ function App() {
 
   return (
     <div className="app">
-     <Header onClickCallback={clearSelection} />
-     {movies.length === 0 && <Home popularCallback={showMostPopular} luckyCallback={showMostLuckyMatch} />}
+      <Header onClickCallback={clearSelection} />
+      {movies.length === 0 && <Home popularCallback={showMostPopular} luckyCallback={showMostLuckyMatch} />}
       {movies.length > 0 && <MovieList pageNum={page} prevCallback={showPrevChunk} nextCallback={showNextChunk} movies={movies} />}
     </div>
   )
