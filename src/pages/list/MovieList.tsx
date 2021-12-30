b0354b22a0c7b858d4cfd0fcaf335a7911269da6
mihail.gaberov <mihail.gaberov@ysura.com>
2021-12-30 11:20:58 +0100

    :lipstick: Styling the movie list

--- src/pages/list/MovieList.tsx
+++ src/pages/list/MovieList.tsx
@@ -54,41 +54,41 @@ import './MovieList.scss'
  */
 
 interface Movie {
-    original_title: string;
-    overview: string;
-    vote_average: string;
-    backdrop_path: string;
+  original_title: string;
+  overview: string;
+  vote_average: string;
+  backdrop_path: string;
 }
 
 interface MovieListProps {
-    movies: Movie[];
+  movies: Movie[];
 }
 
 const API_KEY = import.meta.env.VITE_API_KEY
 
 const MovieList: FunctionComponent<MovieListProps> = ({ movies }) => {
-    console.log(movies)
-    return (
-        <div className='movies-list'>
-            <header>Movies</header>
-            <ul className="table-header">
-                <li></li>
-                <li>Title</li>
-                <li>Description</li>
-                <li>Rating</li>
-                <li>Poster</li>
-            </ul>
-            {movies.map((movie: Movie, idx) => <ul className='movies' key={idx}>
-                <li><span className='bolded'>Title: </span><span>{movie.original_title}</span></li>
-                <li><span className='bolded'>Overview: </span><span>{movie.overview}</span></li>
-                <li><span className='bolded'>Rating: </span><span>{movie.vote_average}</span></li>
-                <li><a href="">
-                    <img src={'http://image.tmdb.org/t/p/w154' + movie.backdrop_path + `?api_key=${API_KEY}`} alt='tsest' />
-                </a>
-                </li>
-            </ul>)}
-        </div>
-    );
+  console.log(movies)
+  return (
+    <div className='movies-list'>
+      <header>Movies</header>
+      <ul className="table-header">
+        <li></li>
+        <li>Title</li>
+        <li>Description</li>
+        <li>Rating</li>
+        <li>Poster</li>
+      </ul>
+      {movies.map((movie: Movie, idx) => <ul className='movies' key={idx}>
+        <li>{idx + 1}.</li>
+        <li>{movie.original_title}</li>
+        <li>{movie.overview}</li>
+        <li>{movie.vote_average}</li>
+        <li><a href={'http://image.tmdb.org/t/p/w500' + movie.backdrop_path + `?api_key=${API_KEY}`}>
+          <img src={'http://image.tmdb.org/t/p/w92' + movie.backdrop_path + `?api_key=${API_KEY}`} alt='tsest' />
+        </a></li>
+      </ul>)}
+    </div>
+  );
 }
 
 export default MovieList;
