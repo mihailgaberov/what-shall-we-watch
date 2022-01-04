66457a9a0adab6ac5576f8400a231a5afade9a05
mihail.gaberov <mihail.gaberov@ysura.com>
2022-01-04 11:00:02 +0100

    :white_check_mark: Add Home tests

---
+++ src/pages/homepage/Home.test.tsx
@@ -0,0 +1,23 @@
+import { describe, expect, it, vitest } from 'vitest'
+import { render, screen, userEvent } from '../../utils/test-utils'
+import Home from './Home'
+
+describe('Home page', () => {
+  it('should render CTA buttons', () => {
+    render(<Home popularCallback={undefined} luckyCallback={undefined} />)
+    expect(screen.getByText('Most Popular')).toBeDefined()
+    expect(screen.getByText('Feeling Lucky')).toBeDefined()
+  })
+
+  it('should be able to click on the CTAs', () => {
+    const popularFn = vitest.fn()
+    const luckyFn = vitest.fn()
+    render(<Home popularCallback={popularFn} luckyCallback={luckyFn} />)
+    const btnMostPopular = screen.getByText('Most Popular')
+    userEvent.click(btnMostPopular)
+    expect(popularFn).toBeCalled()
+    const btnLucky = screen.getByText('Feeling Lucky')
+    userEvent.click(btnLucky)
+    expect(luckyFn).toBeCalled()
+  })
+})
